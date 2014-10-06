3ade2cb18be07d31d2e0203ee7233bf279e6f496
Mwu <hawkyjl@gmail.com>
2014-10-06 11:05:00 +0800

    Revert "hello2"
    This reverts commit 8d5ebb09f36df4d5fd753bbda6681b60a397f6d7.

--- DouBanWithlRC-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <AFNetworking.h>
-#import <DPToastView.h>
-#import <TWMessageBarManager.h>
